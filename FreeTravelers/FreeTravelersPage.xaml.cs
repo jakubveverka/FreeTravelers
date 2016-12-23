57fb72d2946f22907a6b919e9e8d6293a468d6c4
Jakub Veverka <jveverka@oldanygroup.cz>
2016-12-23 13:15:49 +0100

    init commit

---
+++ FreeTravelers/FreeTravelersPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace FreeTravelers
+{
+	public partial class FreeTravelersPage : ContentPage
+	{
+		public FreeTravelersPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
